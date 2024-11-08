f8424d2bfad345038f5e10afaba8c0066451df96
Luciano Oliveira <aulascursos66@gmail.com>
2024-11-08 03:33:13 -0300

    chore: add shadcn setup

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,5 @@
 const Transactions = () => {
-    return ( 
-                    <h1 className="p-5 text-red-500 mb-3">Transactions page</h1>
-     );
-}
- 
+  return <h1 className="mb-3 p-5 text-red-500">Transactions page</h1>;
+};
+
 export default Transactions;
