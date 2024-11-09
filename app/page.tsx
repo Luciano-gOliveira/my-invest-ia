de10785200fc2c5abce760ff65f07bfd6af7cca8
Luciano Oliveira <aulascursos66@gmail.com>
2024-11-09 19:08:49 -0300

    feat: add transaction type badge

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const Home = async () => {
 
   return (
     <div className="flex h-full items-center justify-center">
-      <UserButton showName/>
+      <UserButton showName />
     </div>
   );
 };
